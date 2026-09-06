2b387e7d0fe99c8a78e368030587ab346e9963c6
Dush H <2056742829@qq.com>
2026-09-06 11:28:41 +0800

    chore: main 回退为 HID+SD 稳定版；M3 复合设备归档至 feature/usb-composite

--- USB_DEVICE/App/usb_device.c
+++ USB_DEVICE/App/usb_device.c
@@ -24,7 +24,6 @@
 #include "usbd_core.h"
 #include "usbd_desc.h"
 #include "usbd_hid.h"
-#include "usbd_composite.h"
 
 /* USER CODE BEGIN Includes */
 
@@ -72,7 +71,7 @@ void MX_USB_DEVICE_Init(void)
   {
     Error_Handler();
   }
-  if (USBD_RegisterClass(&hUsbDeviceFS, &USBD_Composite) != USBD_OK)
+  if (USBD_RegisterClass(&hUsbDeviceFS, &USBD_HID) != USBD_OK)
   {
     Error_Handler();
   }
